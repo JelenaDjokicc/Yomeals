bc7ba0132510fd4134ec9d3e78b5bd2174ad02e0
JelenaDjokicc <jeca90ks@gmail.com>
2021-06-29 14:24:42 +0200

    Remove uselless comments

--- src/pages/LocationPopupPage.java
+++ src/pages/LocationPopupPage.java
@@ -12,17 +12,13 @@ public class LocationPopupPage extends BasicPage {
 		super(driver, js, wait);
 	}
 
-//		get metoda za element koji prikazuje lokaciju u hederu 		
 	public WebElement getLocation() {
 		return driver.findElement(By.xpath("//*[@class='location-selector']/a"));
 	}
 
-//		get metodu za close element	
 	public WebElement getCloseElement() {
 		return driver.findElement(By.className("close-btn-white"));
 	}
-
-//			get metode potrebne za implementaciju metode u kojoj se postavlja lokacija
 
 	public WebElement getKeyword() {
 		return driver.findElement(By.xpath("//*[@id='locality_keyword']"));
@@ -31,33 +27,28 @@ public class LocationPopupPage extends BasicPage {
 	public WebElement getLocationItem(String locationName) {
 		return driver.findElement(By.xpath("//li/a[contains(text(), '\" + locationName + \"')]/.."));
 	}
+
 	public WebElement getLocationInput() {
 		return driver.findElement(By.xpath("//*[@id='location_id']"));
 	}
+
 	public WebElement getSubmit() {
 		return driver.findElement(By.xpath("//*[@name='btn_submit']"));
 	}
-//	metodu koja otvara iskačući dijalog
-//	klikom na lokaciju iz hedera
-	public void clickLocation () {
+
+	public void clickLocation() {
 		getLocation().click();
-	}		
-//	metodu koja postavlja lokaciju - naziv lokacije (locationName) se prosleđuje kao parametar metode
-//	metoda prvo klikne na element keyword element
-//	čita vrednost data-value atributa location item elementa	
-//	postavlja lokaciju izvršavajući JavaScript kod
-//	Skripta: arguments[0].value=arguments[1]
-//	prvi argument skripte je location input
-//	drugi argument skripte je vrednost pročitanog atributa iz drugog koraka.
-//	Klikće na submit element preko skripte arguments[0].click();
-	public void setLocation (String locationName) {
+	}
+
+	public void setLocation(String locationName) {
+		getLocation().click();
 		getKeyword().click();
 		String dataValue = getLocationItem(locationName).getAttribute("data-value");
 		js.executeScript("arguments[0].value=arguments[1]", getLocationInput(), dataValue);
 		js.executeScript("arguments[0].click();", getSubmit());
 	}
-//	metodu koja zatvara iskačući dijalog, klikom na X dugme
-	public void closeLocationDialog () {
+
+	public void closeLocationDialog() {
 		getCloseElement().click();
 	}
 
